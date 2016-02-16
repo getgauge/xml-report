7c0d999e0e2cf5f2010ee2ecff59cc31b8429f1b
apoorvam <apoorva_rarefriend@yahoo.com>
2016-02-16 15:00:53 +0530

    Revert "adding nightly info to distro file name"
    This reverts commit 08bd6027442ed2f58e9372daffa127c96719b09e.

--- build/make.go
+++ build/make.go
@@ -30,9 +30,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
-	"time"
-
-	"github.com/getgauge/common"
 )
 
 const (
@@ -59,14 +56,9 @@ const (
 )
 
 var deployDir = filepath.Join(deploy, xmlReport)
-var buildMetadata string
 
 func main() {
 	flag.Parse()
-	if *nightly {
-		buildMetadata = fmt.Sprintf("nightly-%s", time.Now().Format(common.NightlyDatelayout))
-	}
-
 	if *install {
 		updatePluginInstallPrefix()
 		installPlugin(*pluginInstallPrefix)
@@ -100,7 +92,7 @@ func createPluginDistro(forAllPlatforms bool) {
 }
 
 func createDistro() {
-	packageName := fmt.Sprintf("%s-%s-%s.%s", xmlReport, getPluginVersionWithBuildInfo(), getGOOS(), getArch())
+	packageName := fmt.Sprintf("%s-%s-%s.%s", xmlReport, getPluginVersion(), getGOOS(), getArch())
 	distroDir := filepath.Join(deploy, packageName)
 	copyPluginFiles(distroDir)
 	createZipFromUtil(deploy, packageName)
@@ -280,14 +272,6 @@ func getPluginVersion() string {
 	return pluginProperties["version"].(string)
 }
 
-func getPluginVersionWithBuildInfo() string {
-	version := getPluginVersion()
-	if buildMetadata != "" {
-		version += fmt.Sprintf(".%s", buildMetadata)
-	}
-	return version
-}
-
 func moveOSBinaryToCurrentOSArchDirectory(targetName string) {
 	destDir := path.Join(bin, fmt.Sprintf("%s_%s", runtime.GOOS, runtime.GOARCH))
 	moveBinaryToDirectory(path.Base(targetName), destDir)
@@ -319,7 +303,6 @@ var pluginInstallPrefix = flag.String("plugin-prefix", "", "Specifies the prefix
 var distro = flag.Bool("distro", false, "Creates distributables for the plugin")
 var allPlatforms = flag.Bool("all-platforms", false, "Compiles or creates distributables for all platforms windows, linux, darwin both x86 and x86_64")
 var binDir = flag.String("bin-dir", "", "Specifies OS_PLATFORM specific binaries to install when cross compiling")
-var nightly = flag.Bool("nightly", false, "Adds nightly build information")
 
 var (
 	platformEnvs = []map[string]string{
@@ -356,7 +339,7 @@ func compileAcrossPlatforms() {
 
 func installPlugin(installPrefix string) {
 	copyPluginFiles(deployDir)
-	pluginInstallPath := filepath.Join(installPrefix, xmlReport, getPluginVersionWithBuildInfo())
+	pluginInstallPath := filepath.Join(installPrefix, xmlReport, getPluginVersion())
 	mirrorDir(deployDir, pluginInstallPath)
 }
 
