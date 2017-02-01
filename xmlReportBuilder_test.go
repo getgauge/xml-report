045d7f9b8dddc1c4c93c4628c8af0fc56bb3bf3a
Kashishm <kashishmunjal64@gmail.com>
2017-02-01 14:42:58 +0530

    Updating license text

--- xmlReportBuilder_test.go
+++ xmlReportBuilder_test.go
@@ -1,19 +1,19 @@
 // Copyright 2015 ThoughtWorks, Inc.
 
-// This file is part of getgauge/html-report.
+// This file is part of getgauge/xml-report.
 
-// getgauge/html-report is free software: you can redistribute it and/or modify
+// getgauge/xml-report is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// getgauge/html-report is distributed in the hope that it will be useful,
+// getgauge/xml-report is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with getgauge/html-report.  If not, see <http://www.gnu.org/licenses/>.
+// along with getgauge/xml-report.  If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
