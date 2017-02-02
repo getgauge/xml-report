1d1d1e31b678425740dd9d36e3b5866f7b488919
Kashishm <kashishmunjal64@gmail.com>
2017-02-02 11:54:45 +0530

    Adding tests

--- builder/xmlReportBuilder_test.go
+++ builder/xmlReportBuilder_test.go
@@ -319,6 +319,70 @@ func (s *MySuite) TestGetSpecNameWhenHeadingIsNotPresent(c *C) {
 	c.Assert(want, Equals, got)
 }
 
+func (s *MySuite) TestHasParseErrors(c *C) {
+	errors := []*gauge_messages.Error{
+		{Type: gauge_messages.Error_PARSE_ERROR},
+		{Type: gauge_messages.Error_VALIDATION_ERROR},
+	}
+
+	got := hasParseErrors(errors)
+
+	c.Assert(true, Equals, got)
+}
+
+func (s *MySuite) TestHasParseErrorsWithNoErrors(c *C) {
+	errors := []*gauge_messages.Error{}
+
+	got := hasParseErrors(errors)
+
+	c.Assert(false, Equals, got)
+}
+
+func (s *MySuite) TestHasParseErrorsWithOnyValidationErrors(c *C) {
+	errors := []*gauge_messages.Error{
+		{Type: gauge_messages.Error_VALIDATION_ERROR},
+		{Type: gauge_messages.Error_VALIDATION_ERROR},
+	}
+
+	got := hasParseErrors(errors)
+
+	c.Assert(false, Equals, got)
+}
+
+func (s *MySuite) TestGetErrorTestCase(c *C) {
+	res := &gauge_messages.ProtoSpecResult{
+		ProtoSpec: &gauge_messages.ProtoSpec{
+			SpecHeading: "heading",
+		},
+		Errors: []*gauge_messages.Error{
+			{
+				Type:    gauge_messages.Error_PARSE_ERROR,
+				Message: "parse error",
+			},
+			{
+				Type:    gauge_messages.Error_VALIDATION_ERROR,
+				Message: "validation error",
+			},
+		},
+		ExecutionTime: int64(1),
+	}
+
+	want := JUnitTestCase{
+		Classname: "heading",
+		Name:      "heading",
+		Time:      formatTime(int(1)),
+		Failure: &JUnitFailure{
+			Message:  "Parse/Validation Errors",
+			Type:     "Parse/Validation Errors",
+			Contents: "[Parse Error] parse error\n[Validation Error] validation error",
+		},
+	}
+
+	got := getErrorTestCase(res)
+
+	c.Assert(want, DeepEquals, got)
+}
+
 func assertXmlValidation(xml []byte, c *C) {
 	doc, err := libxml2.Parse(xml, parser.XMLParseNoNet)
 	c.Assert(err, Equals, nil)
