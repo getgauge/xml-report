ec2d5c2447bc17c1f50264742f3364acc85ee38b
BugDiver <vinaysh@thoughtworks.com>
2019-12-02 18:51:50 +0530

    Using gRPC for communication with gauge.

--- main.go
+++ main.go
@@ -17,11 +17,32 @@
 
 package main
 
-import "os"
+import (
+	"net"
+	"os"
+
+	"github.com/getgauge/xml-report/logger"
+
+	gm "github.com/getgauge/xml-report/gauge_messages"
+	"google.golang.org/grpc"
+)
 
 func main() {
 	findPluginAndProjectRoot()
-	if os.Getenv(PLUGIN_ACTION_ENV) == EXECUTION_ACTION {
-		createExecutionReport()
+	if os.Getenv(pluginActionEnv) == executionAction {
+		os.Chdir(projectRoot)
+		address, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
+		if err != nil {
+			logger.Fatal("failed to start server.")
+		}
+		l, err := net.ListenTCP("tcp", address)
+		if err != nil {
+			logger.Fatal("failed to start server.")
+		}
+		server := grpc.NewServer(grpc.MaxRecvMsgSize(1024 * 1024 * 1024 * 10))
+		h := &handler{server: server}
+		gm.RegisterReporterServer(server, h)
+		logger.Info("Listening on port:%d", l.Addr().(*net.TCPAddr).Port)
+		server.Serve(l)
 	}
 }
