1a9c05cee03dc8d191983a5a4c2df4044b268461
sriv <srikanth.ddit@gmail.com>
2020-05-19 16:32:15 +0530

    fix build flags
    libxml dependency does not work on windows
    
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- builder/integration_test.go
+++ builder/integration_test.go
@@ -1,10 +1,10 @@
+// +build linux darwin
+
 /*----------------------------------------------------------------
  *  Copyright (c) ThoughtWorks, Inc.
  *  Licensed under the Apache License, Version 2.0
  *  See LICENSE in the project root for license information.
  *----------------------------------------------------------------*/
-
-// +build linux darwin
 
 package builder
 
